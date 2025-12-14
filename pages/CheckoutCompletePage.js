a3f2188dd44975f8f77dc06fce04b377fe52de5b
Netanel Harush <netanelharush2@gmail.com>
2025-12-14 16:46:26 +0200

    Refactor user data structure, improve page class formatting, and enhance test organization with URL constants

--- pages/CheckoutCompletePage.js
+++ pages/CheckoutCompletePage.js
@@ -1,19 +1,19 @@
 export class CheckoutCompletePage {
-    constructor(page) {
-        this.page = page;
-        // Locators
-        this.pageTitle = page.locator('.title');
-        this.completeHeader = page.locator('.complete-header');
-        this.backHomeButton = page.locator('#back-to-products');
-    }
-    // Actions / Methods
-    async getPageTitle() {
-        return await this.pageTitle.innerText();
-    }
-    async getCompleteMessage() {
-        return await this.completeHeader.innerText();
-    }
-    async clickBackHome() {
-        await this.backHomeButton.click();
-    }
+  constructor(page) {
+    this.page = page
+    // Locators
+    this.pageTitle = page.locator('.title')
+    this.completeHeader = page.locator('.complete-header')
+    this.backHomeButton = page.locator('#back-to-products')
+  }
+  // Actions / Methods
+  async getPageTitle() {
+    return await this.pageTitle.innerText()
+  }
+  async getCompleteMessage() {
+    return await this.completeHeader.innerText()
+  }
+  async clickBackHome() {
+    await this.backHomeButton.click()
+  }
 }
--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,22 +1,22 @@
 export class LoginPage {
-    constructor(page) {
-        this.page = page;
-        // Locators
-this.usernameInput = page.locator('#user-name');
-this.passwordInput = page.locator('#password');
-this.loginButton = page.locator('#login-button');
-this.errorMessage = page.locator('[data-test="error"]');
-    }
-    // Actions / Methods
-    async navigate(baseUrl) {
-        await this.page.goto(baseUrl);
-    }
-    async login(username, password) {
-        await this.usernameInput.fill(username);
-        await this.passwordInput.fill(password);
-        await this.loginButton.click();
-    }
-    async getErrorMessage() {
-        return await this.errorMessage.innerText();
-    }
+  constructor(page) {
+    this.page = page
+    // Locators
+    this.usernameInput = page.locator('#user-name')
+    this.passwordInput = page.locator('#password')
+    this.loginButton = page.locator('#login-button')
+    this.errorMessage = page.locator('[data-test="error"]')
+  }
+  // Actions / Methods
+  async navigate(baseUrl) {
+    await this.page.goto(baseUrl)
+  }
+  async login(username, password) {
+    await this.usernameInput.fill(username)
+    await this.passwordInput.fill(password)
+    await this.loginButton.click()
+  }
+  async getErrorMessage() {
+    return await this.errorMessage.innerText()
+  }
 }
